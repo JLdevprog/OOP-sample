1927881439167c005cc26940c04a3c1adc1ae333
Johann Legland <Shugox@Johanns-MacBook-Pro.local>
2019-03-08 09:10:21 +0100

    oop updt

--- oop.php
+++ oop.php
@@ -1,2 +1,19 @@
 <?php
 
+//Create object as main object
+
+class character{
+
+	protected $id;
+	protected $name;
+	protected $life_p;
+	protected $attack_p;
+	protected $defence_p;
+	protected $speed_p;
+	protected $power_p;
+	protected $xp_p;
+
+	public function __construct(){
+		
+	}
+}
